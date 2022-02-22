a2df10034a80323eca2499ce75063e1a0eb6512b
FabiaAfzal <fabiaafzal1995@gmail.com>
2022-02-22 19:52:40 +0100

    First Commit

---
+++ maven-project/src/main/java/com/calculator/Application.java
@@ -0,0 +1,9 @@
+package com.calculator;
+
+public class Application {
+	
+	public String Welcome()
+	{
+		return "Hello";
+	}
+}
---
+++ maven-project/src/test/java/com/calculator/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
