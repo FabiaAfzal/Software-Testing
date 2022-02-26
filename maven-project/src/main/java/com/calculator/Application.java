3dec120b18425262caec034e336dae01569a4225
FabiaAfzal <fabiaafzal1995@gmail.com>
2022-02-27 00:16:01 +0100

    write methods along with unit/integration test cases.

--- maven-project/src/main/java/com/calculator/Application.java
+++ maven-project/src/main/java/com/calculator/Application.java
@@ -2,9 +2,19 @@ package com.calculator;
 
 public class Application {
 	
+	public Application() {
+		
+	}
 	public String Welcome()
 	{
 		return "Welcome to Calculator App";
 	}
-	
+	public int addition(int a, int b)
+	{
+		return a + b;
+	}
+	public int subtraction(int a, int b)
+	{
+		return a - b;
+	}
 }
--- maven-project/src/test/java/com/calculator/ApplicationTest.java
+++ maven-project/src/test/java/com/calculator/ApplicationTest.java
@@ -3,14 +3,35 @@ package com.calculator;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.BeforeClass;
 
 public class ApplicationTest {
-
+	
+	public static Application calApplication;
+	
+	@BeforeClass
+	public static void setUp() {
+		calApplication= new Application();
+	}
+	
 	@Test
 	public void applicationTest()
 	{
 		
 		assertTrue(true);
 	}
-
+	@Test
+	public void testadd()
+	{
+		assertEquals(5,calApplication.addition(3,2));
+	}
+	@Test
+	public void testsubtraction()
+	{
+		assertEquals(1,calApplication.subtraction(3,2));
+	}
+	@Test
+	public void testintegration() {
+		assertEquals(6,calApplication.subtraction(calApplication.addition(4,4),calApplication.addition(1,1)));
+	}
 }
